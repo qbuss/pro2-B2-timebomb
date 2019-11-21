986fc3bb620f169b7a2196e3fade601460764da3
qbuss <kuba.farionn@gmail.com>
2019-11-21 12:44:17 +0100

    initial

---
+++ time bomb/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace time_bomb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
