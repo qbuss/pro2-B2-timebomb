23f2b5ff6c487dca11cff5f4b19e3be967e9013e
qbuss <kuba.farionn@gmail.com>
2019-11-26 09:12:27 +0100

    klaar

--- time bomb/Program.cs
+++ time bomb/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
-
-namespace time_bomb
+using System.Timers;
+public class Example
 {
-    class Program
+    static System.Timers.Timer aTimer;
+
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        SetTimer();
+
+        Console.WriteLine("bomb is begonnen om "+ DateTime.Now.Hour + ":" + DateTime.Now.Minute);
+        Console.WriteLine("bomb gaat elke 10 seconden over. Click enter om eruit te gaan");
+        Console.ReadLine();
+        aTimer.Stop();
+        aTimer.Dispose();
+
+        Console.WriteLine("Terminating the application...");
+    }
+
+    static void SetTimer()
+    {
+        // 10 seconden (tijd staat in ms)
+        aTimer = new System.Timers.Timer(10000);
+        aTimer.Elapsed += OnTimedEvent;
+        aTimer.AutoReset = true;
+        aTimer.Enabled = true;
+    }
+
+    private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+    {
+        Console.WriteLine("BOOOM!!!! " + DateTime.Now.Second);
     }
 }
