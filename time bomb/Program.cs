c49190c6eb05b74ece713a580fe10086ee36485b
qbuss <kuba.farionn@gmail.com>
2019-11-26 09:49:20 +0100

    klaar (nu vooor real)

--- time bomb/Program.cs
+++ time bomb/Program.cs
@@ -3,11 +3,12 @@ using System.Timers;
 public class Example
 {
     static System.Timers.Timer aTimer;
+    static int aftellen = 10;
 
     static void Main()
     {
         SetTimer();
-
+        
         Console.WriteLine("bomb is begonnen om "+ DateTime.Now.Hour + ":" + DateTime.Now.Minute);
         Console.WriteLine("bomb gaat elke 10 seconden over. Click enter om eruit te gaan");
         Console.ReadLine();
@@ -20,14 +21,25 @@ public class Example
     static void SetTimer()
     {
         // 10 seconden (tijd staat in ms)
-        aTimer = new System.Timers.Timer(10000);
+        aTimer = new System.Timers.Timer(1000);
         aTimer.Elapsed += OnTimedEvent;
         aTimer.AutoReset = true;
         aTimer.Enabled = true;
     }
 
-    private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+    static void OnTimedEvent(Object source, ElapsedEventArgs e)
     {
-        Console.WriteLine("BOOOM!!!! " + DateTime.Now.Second);
+        
+        switch (aftellen)
+        {
+            case 0:
+                Console.WriteLine("BOOM!!");
+                break;
+            default:
+                Console.WriteLine(aftellen + "sec tot explosie");
+                aftellen--;
+                break;
+        }
+
     }
 }
